85bd0b928f22c71b480072d7036d730a9f92740a
Heba <sweet.bebe67@gmail.com>
2020-09-23 12:40:22 -0400

    add form

--- routes/index.js
+++ routes/index.js
@@ -26,17 +26,81 @@ router.post('/form', async function(req, res, next) {
   res.redirect('/');
 });
 
-router.get('/form', async function(req, res, next) {
-  // text = `SELECT FROM * products`;
-  // rows = [];
-  // try{
-  //   const rows = await database.query(text);
-  // }catch(error){
-  //   console.log(error);
-  // }
-  // console.log(rows)
-  //get the pic form products to show it in the cards
-  res.render('form', { title: 'bysell',cards: ''});
+router.get('/form', function(req, res, next) {
+  
+  res.render('form', { title: 'bysell',});
+});
+
+router.get('/product/:id', function(req, res, next){
+  //id = req.params.id;
+
+});
+
+router.get('/signup', function (req, res, next) {
+  res.render('signup', { pageName: 'signup' });
+});
+
+router.post('/signup', async function(req, res, next){
+  const name = req.body.name;
+  const email= req.body.email;
+  const password= req.body.password;
+
+  query = `INSERT INTO users (name, email, password)
+    VALUES('${name}', '${email}', '${password}')`;
+  try{
+    await database.query(query);
+  }catch(error){
+    console.log(error);
+  }
+  res.redirect('/login')
+});
+
+router.get('/login', function(req, res, next){
+
+
+  res.render('login', {pageName:'',})
+});
+
+router.post('/login',async function(req, res, next){
+
+  const check = req.body.check;
+  
+  text = `SELECT name,id FROM users WHERE
+   email = '${req.body.email}'AND password = '${req.body.password}'`;
+  rows =[];
+   try{
+    var {rows, rowCount} = await database.query(text);
+    if ( rowCount == 0 ){
+      res.redirect('/login');
+      return;
+    }
+   }catch(error){
+    console.log(error);
+   }
+   console.log(rows);
+   req.session.user = rows[0];
+   console.log(req.session.user);
+
+   if (check) {
+    res.cookie('user', req.session.user);
+  }
+
+   res.redirect('/profile');
+});
+
+router.get('/logout', function(req, res, next){
+ req.session.destroy (function(){
+  console.log("user logged out.");
+ });
+ res.clearCookie('user');
+ res.redirect('/login')
+
+  res.render('logout', {pageName:'',})
+});
+router.get('/profile', function(req, res, next){
+
+
+  res.render('profile', {pageName:'profile',})
 });
 
 module.exports = router;
